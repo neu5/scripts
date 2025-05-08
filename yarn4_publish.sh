3c36eb4e2470c55ef5adaf1ac900cb32e0e6c99d
neu5 <neus90@gmail.com>
2025-05-08 19:27:12 +0200

    Update yarn4_publish.sh

--- yarn4_publish.sh
+++ yarn4_publish.sh
@@ -1,8 +1,15 @@
-COMMIT_MSG=$(git diff --name-only | grep package.json | while read file; do
+JIRA_ID="JIRA-123"
+CUSTOM_MSG="Custom message"
+
+CHANGELOG=$(git diff --name-only | grep package.json | while read file; do
   name=$(grep '"name"' "$file" | head -n 1 | sed -E 's/.*"([^"]+)".*/\1/')
   version=$(grep '"version"' "$file" | head -n 1 | sed -E 's/.*"([^"]+)".*/\1/')
-  echo "$name@$version"
-done | sort | paste -sd'\n' -)
+  echo "- $name@$version"
+done | sort)
+
+COMMIT_MSG="[$JIRA_ID] $CUSTOM_MSG
+
+$CHANGELOG"
 
 git add .
 git commit -m "$COMMIT_MSG"
